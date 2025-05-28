70d9ed622d8e4111f2d56fce215e283352b062cf
Myk78 <nwbyaseen@gmail.com>
2025-05-28 23:53:04 +0500

    add /products route with dummy data

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ app.get("/status", (req, res) => {
   const currentTime = new Date().toString();
   return res.json({ status: "ok", time: currentTime });
 });
+app.get("/product", (req, res) => {
+  return res.json([
+    {
+      id: 1,
+      name: "Laptop",
+      price: 1200,
+    },
+    {
+      id: 2,
+      name: "Headphones",
+      price: 150,
+    },
+    {
+      id: 3,
+      name: "Keyboard",
+      price: 80,
+    },
+  ]);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
